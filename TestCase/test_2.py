1abcc0581b6fbec622721e3f940a8c5752a535bc
chenyongyan <975216162@qq.com>
2019-06-04 15:43:55 +0800

    修改test_2.py

--- TestCase/test_2.py
+++ TestCase/test_2.py
@@ -1,32 +1,34 @@
+# encoding:utf-8
 
-from Public_method import MySql
-import json,unittest,re,time
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.by import By
-from Public_method import MySql
-import traceback
-
-file = open("E:\\App_AutoTest\\Public_file\\date.json", "r", encoding="UTF-8")
-dict = json.load(file)
+import json, unittest, re, time
+from ddt import ddt, data, unpack, file_data
+import itertools
 
 
+@ddt
 class case(unittest.TestCase):
 
-    # 清空注册用户数据
     def test1(self):
-        sql1 = "DELETE FROM saas_member_test_20190428.MEM_UserBase WHERE Phone = '13424474182'"
-        sql2 = "update dbo.Mall_Leaguer set Phone=null where Phone='13424474182'"
-        sql3 = "update dbo.Mall_LeaguerApply set PhoneNumber=null where PhoneNumber='13424474182'"
-        sql4 = "DELETE FROM dbo.Ykb_NewMembersGivingLog WHERE Phone = '13424474182'"
+        A = ['1', '2']
+        B = ['a', 'b', 'c']
 
-        MySql.db_NotQuery_200(self,'saas_member_test_20190428',sql1)
-        MySql.db_NotQuery_233(self,'YCHMALL',sql2)
-        MySql.db_NotQuery_233(self,'YCHMALL',sql3)
-        MySql.db_NotQuery_233(self,'YCHMALL',sql4)
+        for x in itertools.product(A, B):
+            print(x)
+
+    @data('1', '2', '3')
+    def test2(self, keys):
+        print(keys)
+
+    @data(['a', 'b'], ['c', 'd'])
+    @unpack
+    def test3(self, values, keys):
+        # print(values)
+        print(keys)
+
+    @file_data(r'E:\App_AutoTest\Public_file\test.json')
+    def test4(self, values):
+        print(values)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
