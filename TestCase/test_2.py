3080897b6d6e8a190663e7eead3548b34c256709
chenyongyan <975216162@qq.com>
2019-06-10 18:09:13 +0800

    test2.py遍历查询

--- TestCase/test_2.py
+++ TestCase/test_2.py
@@ -1,32 +1,68 @@
 # encoding:utf-8
 
-import json, unittest, re, time
+import unittest,time
 from ddt import ddt, data, unpack, file_data
 import itertools
+from selenium import webdriver
 
 
 @ddt
 class case(unittest.TestCase):
 
-    def test1(self):
-        A = ['1', '2']
-        B = ['a', 'b', 'c']
 
-        for x in itertools.product(A, B):
-            print(x)
+    def test1(self):
+        self.dr = webdriver.Chrome()
+        self.dr.maximize_window()
+        self.dr.get('http://172.16.0.161:9999/admin')
+        self.dr.implicitly_wait(20)
+
+        self.dr.find_element_by_id('txtName').send_keys('admin')
+        self.dr.find_element_by_id('txtPwd').send_keys('1')
+        self.dr.find_element_by_id('fm-login-submit').click()
+        time.sleep(2)
+        self.dr.find_element_by_id('mumu_group100001400').click()
+        time.sleep(1)
+        self.dr.find_element_by_id('left_100001400').click()
+        time.sleep(1)
+        self.dr.find_element_by_id('left_101016100').click()
+        time.sleep(3)
+        self.dr.switch_to_frame(1)
+
+        # 会员卡号
+        #self.dr.find_element_by_id("txtLeaguerCode").send_keys(phone)
+        # 会员姓名
+        #self.dr.find_element_by_id("txtLeaguergName").send_keys(name)
+
+
+
+        # 账期
+        a = ['//*[@id="asd1fgh3jkldll1"]/div/div/ul/li[1]','//*[@id="asd1fgh3jkldll1"]/div/div/ul/li[2]','//*[@id="asd1fgh3jkldll1"]/div/div/ul/li[3]','//*[@id="asd1fgh3jkldll1"]/div/div/ul/li[4]']
+        # 类型
+        b = ['//*[@id="IsChange"]/div/div/ul/li[2]', '//*[@id="IsChange"]/div/div/ul/li[3]']
+        for x in itertools.product(a, b):
+
+            self.dr.find_element_by_xpath('//*[@id="asd1fgh3jkldll1"]/div/div').click()
+            time.sleep(1)
+            self.dr.find_element_by_xpath(x[0]).click()
+
+            self.dr.find_element_by_xpath('//*[@id="IsChange"]/div/div/button').click()
+            time.sleep(1)
+            self.dr.find_element_by_xpath(x[1]).click()
+
+            self.dr.find_element_by_xpath('/html/body/div[1]/div[6]/button[1]').click()
 
     @data('1', '2', '3')
     def test2(self, keys):
         print(keys)
 
-    @data(['a', 'b'], ['c', 'd'])
+    @data(['value_1','key_1'], ['value_2','key_2'],['value_3','key_3'],['value_4','key_4'])
     @unpack
     def test3(self, values, keys):
         # print(values)
         print(keys)
 
     @file_data(r'E:\App_AutoTest\Public_file\test.json')
-    def test4(self, values):
+    def test4(self,values):
         print(values)
 
 
